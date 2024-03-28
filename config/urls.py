17ed2c98778e7797ff3f5220cabcd513c7492f94
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2024-03-28 12:55:55 -0300

    first

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+"""
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('todo.urls'))
+]
