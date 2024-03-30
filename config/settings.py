b00642787fe08b59a3d5912a311d8107844ddd20
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2024-03-30 12:49:17 -0300

    Disables browsable api entirely

--- config/settings.py
+++ config/settings.py
@@ -58,8 +58,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_RENDERER_CLASSES": (
-    "rest_framework.renderers.JSONRenderer",
-    "todo.renderers.CustomBrowsableAPIRenderer",
+        "rest_framework.renderers.JSONRenderer",
     )
 }
 
