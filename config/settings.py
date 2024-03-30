cc52162a49f546fb7bdc46fcd747ec7aae4c23fb
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2024-03-30 13:02:48 -0300

    Removes unused session auth

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,6 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_RENDERER_CLASSES": (
