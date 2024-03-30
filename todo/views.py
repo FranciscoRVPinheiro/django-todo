8fd483eee7aa111d94c3c093511a0f2679704e2e
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2024-03-30 11:26:07 -0300

    Adds jwt decoding in views

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from .serializers import BaseTasksSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .permissions import IsOwner
+from django.conf import settings
+import jwt
 
 class ListUsers(generics.ListAPIView):
     queryset = User.objects.all()
@@ -21,6 +23,17 @@ class CreateTask(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = BaseTasksSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        token = self.request.headers.get('Authorization').split(' ')[1]
+        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        user = User.objects.get(id=decoded_token['user_id'])
+        serializer.save(user=user)
+
+    def create(self, request, *args, **kwargs):
+        request.data['user'] = self.request.user.id
+        return super().create(request, *args, **kwargs)
+
 
 class TaskDetail(generics.RetrieveAPIView):
     queryset = Task.objects.all()
